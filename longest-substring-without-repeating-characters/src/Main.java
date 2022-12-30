bcd13fac89805d57f863e281f6a9b239f589f99f
bimales <mail2bimales@gmail.com>
2022-12-30 15:42:38 +0530

    final commit

--- longest-substring-without-repeating-characters/src/Main.java
+++ longest-substring-without-repeating-characters/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s = "abcha";
+        System.out.println(longestSubstringWithoutRepeatingCharacters(s));
+    }
+
+    private static int longestSubstringWithoutRepeatingCharacters(String s) {
+        if (s==null) return 0;
+        Set<Character> set = new HashSet<>();
+        int left = 0, right = 0, maxLength =0;
+        while(right < s.length()){
+            char current = s.charAt(right);
+            if( !set.contains(current)){
+               set.add(current);
+               right++;
+               maxLength = Math.max(maxLength, right - left);
+            }else{
+                char toRemove = s.charAt(left);
+                set.remove(toRemove);
+                left++;
+            }
+        }
+        return maxLength;
     }
 }
